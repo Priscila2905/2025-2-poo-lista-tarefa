60456acefd9abd15f3ee7a0b394b799b0b3a43d6
Priscila2905 <priscilapaivaiglesias@gmail.com>
2025-10-07 00:04:32 +0000

    tarefa

---
+++ Tarefa.cs
@@ -0,0 +1,12 @@
+public class tarefa 
+{
+
+    public id int 
+    public nome string
+    public descricao string
+    public datacriacao Datetime
+    public status int 
+    public dataExecucao Datetime
+ 
+
+ 
